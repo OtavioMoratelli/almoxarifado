f1f46ee89914a44b4bb7f621eeffbaa090923e41
Otávio <50156051+OtavioMoratelli@users.noreply.github.com>
2023-06-20 01:05:25 -0300

    Login&Logout

--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .models import Item
 
 def home(request):
@@ -7,4 +9,20 @@ def home(request):
 
     return render(request, 'home.html', {'queryResult':queryResult})
 
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Você entrou em sua conta com sucesso!")
+            return redirect('home')
+        else:
+            messages.error(request, "Erro ao entar, tente novamente...")
 
+    return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    return redirect('home') 
